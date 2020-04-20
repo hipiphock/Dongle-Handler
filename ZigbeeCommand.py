6506c115923a4e0294d78b04b884d26c712ba8e9
hingook@splab <hipiphock@naver.com>
2020-04-20 17:45:58 +0900

    finished making brief clusters

--- ZigbeeCommand.py
+++ ZigbeeCommand.py
@@ -2,10 +2,31 @@ import enum
 
 # maps cluster value into 
 # which function does it is concerned with
-class Cluster(enum.Enum):
-    OnOff = 0x0006
-    Level = 0x0008
-    Color = 0x0300
+class Cluster:
+    def __init__(self, clusterid):
+        # OnOff = 0x0006
+        # Level = 0x0008
+        # Color = 0x0300
+        self.clusterid = clusterid
+    
+    # On & Off Cluster
+    class OnOff:
+        def __init__(self, commandid):
+            super().__init__()
+            self.commandid = commandid
+
+    # Controls the brightness of the light
+    class LevelControl:
+        def __init__(self, commandid, payloads):
+            super().__init__()
+            self.commandid = commandid
+    
+    # Controls the temperature
+    class ColorControl:
+        def __init__(self, commandid, payloads):
+            super().__init__()
+            self.commandid = commandid
+
 
 # Each config should be one Zigbee command,
 # since each Zigbee command is a transaction

69b963b95a2055d263eb4e22fefa5f9460d16ce7
hingook@splab <hipiphock@naver.com>
2020-04-27 17:21:36 +0900

    not working

--- main.py
+++ main.py
@@ -1,35 +1,179 @@
-# MADE FOR TEST
+import json
+import random
 import serial
-import ZigbeeCommand
-import BLECommand
 from zb_cli_wrapper.zb_cli_dev import ZbCliDevice
 from zb_cli_wrapper.src.utils.cmd_wrappers.zigbee.constants import *
-from zb_cli_wrapper.src.utils.zigbee_classes.clusters.attribute import Attribute
 
-# main routine for zigbee connection
-def main():
+ZIGBEE_STARTED = False
+cli_instance = None
 
-    eui64 = 9824354097448244246
+# finds the port that dongle is inserted
+def find_dongle_port():
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        # find the appropriate port based on the name of the dongle
+        # TODO: Implement automated port selector
+        pass
 
-    # first, try to connect with dongle
-    # try:
-    cli_instance = ZbCliDevice('', '','COM13')    # COM port can be changed in accordance with your environment
-    #     cli_instance.bdb.channel = 24
-    #     cli_instance.bdb.role = 'zr'
-    # except serial.serialutil.SerialException:
-    #     print('Can not create CLI device')
-    #     cli_instance.close_cli()
-    #     return None
+# representing command.json file,
+# CommandSet defines:
+# - Device's information
+# - Sequences of commands and its number of iterations
+class CommandSet:
+    def __init__(self, name, uuid, addr, ep, cmdlist):
+        self.name = name
+        self.uuid = uuid
+        self.addr = addr
+        self.ep = ep
+        self.cmdlist = cmdlist
+    # in this program, instance of CommandSet should be generated via
+    # this classmethod.
+    @classmethod
+    def make_instance(cls, cmdfile):
+        with open(cmdfile) as commandfile:
+            content = json.load(commandfile)
+            name    = content['Device']
+            uuid    = content['uuid']
+            addr    = content['address']
+            ep      = content['ep']
+            cmds    = content['command_list']
+            cmdlist = []
+            for cmd in cmds:
+                configfile  = cmd['command'] + ".json"
+                iteration   = cmd['iteration']
+                config = Config.make_instance(configfile)
+                cmdlist.append({'config':config, 'iteration':iteration})
+        return cls(name, uuid, addr, ep, cmdlist)
 
-    # print("Created CLI device, trying to connect ...")
-    # cli_instance.bdb.start()
-    # print(cli_instance.wait_until_connected())
+    # main procedure of command.json
+    def start_routine(self):
+        for command in self.cmdlist:
+            config      = command['config']
+            iteration   = command['iteration']
+            for i in range(iteration):
+                self.do_individual_job(config)
+            # global cli_instance
+            # cli_instance.close_cli()
+            print("command routine finished")
 
-    # attribute = Attribute(ON_OFF_CLUSTER, 0, TYPES.BOOL, ON_OFF_OFF_CMD)
-    # cli_instance.zcl.generic(eui64, 8, ON_OFF_CLUSTER, DEFAULT_ZIGBEE_PROFILE_ID, ON_OFF_OFF_CMD)
-    # cli_instance.zcl.generic(eui64, 8, ON_OFF_CLUSTER, DEFAULT_ZIGBEE_PROFILE_ID, ON_OFF_ON_CMD)
-    cli_instance.zcl.raw(eui64, 8, ON_OFF_CLUSTER, '0x00')
+    # does the individual process of Zigbee command
+    # or BLE service
+    def do_individual_job(self, config):
+        if config.connection == 'Zigbee':
+            global ZIGBEE_STARTED
+            global cli_instance
+            # if ZIGBEE_STARTED == False:
+            #     try:
+            #         print("Starting Zigbee Connection...")
+            cli_instance = ZbCliDevice('','','COM13')
+            #         cli_instance.bdb.channel = [24]
+            #         cli_instance.bdb.role = 'zr'
+            #         cli_instance.bdb.start()
+            #         ZIGBEE_STARTED = True
+            #     except serial.serialutil.SerialException:
+            #         cli_instance.close_cli()
+            #         return None
+            attribute = make_attr(self.addr, self.ep,
+                    config.command, config.payloads)
+            print("address: {}".format(attribute.eui64))
+            print("endpoint: {}".format(attribute.ep))
+            print("cluster: {}".format(attribute.cluster))
+            print("cmd: {}".format(attribute.cmd_id))
+            print("payloads: {}".format(attribute.payload))
+            if attribute.payload == []:
+                cli_instance.zcl.generic(
+                        eui64= attribute.eui64, 
+                        ep = attribute.ep,
+                        profile=DEFAULT_ZIGBEE_PROFILE_ID,
+                        cluster=attribute.cluster, 
+                        cmd_id=attribute.cmd_id)                
+            else:
+                cli_instance.zcl.generic(
+                        eui64= attribute.eui64, 
+                        ep = attribute.ep, 
+                        profile=DEFAULT_ZIGBEE_PROFILE_ID, 
+                        cluster=attribute.cluster, 
+                        cmd_id=attribute.cmd_id, 
+                        payload=attribute.payload)
+
+# class representing config.json files
+# each config file's should be made by this inner class
+class Config:
+    def __init__(self, connection, command, payloads=None):
+        self.connection = connection
+        self.command = command
+        self.payloads = payloads
+    
+    @classmethod
+    def make_instance(cls, configfile):
+        with open(configfile) as config_file:
+            content = json.load(config_file)
+            connection  = content['connection']
+            command     = content['command']
+            payload     = content['payloads']
+            payloads    = format_payload(payload)
+            # additional job required for payloads:
+            # - the generic function gets payloads as lists of tuples
+        return cls(connection, command, payloads)
+        
+# defines Zigbee Attribute
+class ZigbeeAttr:
+    def __init__(self, _eui64, _ep, _cluster, _cmd_id, _payload):
+        self.eui64 = _eui64
+        self.ep = _ep
+        self.cluster = _cluster
+        self.cmd_id = _cmd_id
+        self.payload = _payload
+        
+def make_attr(address, ep, command, payloads):
+    command_map = { "ON_OFF_OFF_CMD":   ON_OFF_OFF_CMD, 
+                    "ON_OFF_ON_CMD":    ON_OFF_ON_CMD,
+                    "LVL_CTRL_MV_TO_LVL_CMD":   LVL_CTRL_MV_TO_LVL_CMD,
+                    "COLOR_CTRL_MV_TO_HUE_CMD":     COLOR_CTRL_MV_TO_HUE_CMD,
+                    "COLOR_CTRL_MV_TO_SAT_CMD":     COLOR_CTRL_MV_TO_SAT_CMD,
+                    "COLOR_CTRL_MV_TO_HUE_SAT_CMD": COLOR_CTRL_MV_TO_HUE_SAT_CMD}
+    command_int = command_map[command]
+    cluster = 0
+    if command.find("ON_OFF") != -1:
+        cluster = ON_OFF_CLUSTER
+    elif command.find("LVL_CTRL") != -1:
+        cluster = LVL_CTRL_CLUSTER
+    elif command.find("COLOR_CTRL") != -1:
+        cluster = COLOR_CTRL_CLUSTER
+    addr = int(address, 16)
+    return ZigbeeAttr(addr, ep, cluster, command_int, payloads)
+
+def format_payload(payload):
+    if payload == "None":
+        return
+    types_map = {
+        "TYPES.BOOL": TYPES.BOOL,
+        "TYPES.UINT8": TYPES.UINT8,
+        "TYPES.UINT16": TYPES.UINT16, 
+        "TYPES.UINT32": TYPES.UINT32,
+        "TYPES.UINT64": TYPES.UINT64,
+        "TYPES.SINT8": TYPES.SINT8,
+        "TYPES.SINT16": TYPES.SINT16,
+        "TYPES.SINT64": TYPES.SINT64, 
+        "TYPES.ENUM8": TYPES.ENUM8,
+        "TYPES.MAP8": TYPES.MAP8, 
+        "TYPES.EUI64": TYPES.EUI64,
+        "TYPES.STRING": TYPES.STRING }
+    result = []
+    for item in payload:
+        value_type = types_map[item['type']]
+        if value_type is not TYPES.STRING:
+            value = int(item['value'], 16)
+        else:
+            value = item['value']
+        result.append((value, value_type))
+    return result
 
 
 if __name__ == "__main__":
-    main()
+    # find_dongle_port()
+    commander = CommandSet.make_instance('command1.json')
+    commander.start_routine()
+    # ser = serial.Serial('COM13')
+    # ser.write(b'reset')
+    # ser.close()

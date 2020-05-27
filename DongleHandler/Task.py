96d3600a850a0cbccacf63854536016c68537780
hingook@splab <hipiphock@naver.com>
2020-05-27 17:52:55 +0900

    코드 공사중입니다.

---
+++ DongleHandler/Task.py
@@ -0,0 +1,8 @@
+# Task is an element for each taskRoutine to have for
+
+class Task:
+    def __init__(self, command, payloads):
+        self.command = command
+        self.payloads = payloads
+    
+    def 
--- DongleHandler/TaskRoutine.py
+++ DongleHandler/TaskRoutine.py
@@ -1,6 +1,24 @@
 from Device import *
+from zb_cli_wrapper.zb_cli_dev import ZbCliDevice
 
 # Work Routine for
 class TaskRoutine:
-    def __init__(self, device):
+    def __init__(self, device, connection_type, task_list):
         self.device = device
+        self.connection_type = connection_type
+        self.task_list = task_list
+    
+    def start_routine():
+        # each task routine starts with connection
+        cli_instance = ZbCliDevice('','','COM13')
+        cli_instance.bdb.channel = [24]
+        cli_instance.bdb.role = 'zr'
+        cli_instance.bdb.start()
+        # TODO: implement automated method that does not
+        # require SmartThings' permission.
+
+        # do the task_list
+        
+
+        # each task routine ends with disconnection
+        
